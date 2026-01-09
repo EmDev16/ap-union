fa23810567f34a293ed75ecbf5f15e7a418751bb
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-09 22:33:48 +0100

    maakt PromptSubmission model met relationships naar Prompt, User, en Post

--- app/Models/PromptSubmission.php
+++ app/Models/PromptSubmission.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class PromptSubmission extends Model
 {
-    //
+    protected $fillable = [
+        'prompt_id',
+        'user_id',
+        'content',
+        'media_path',
+        'scheduled_publish_at',
+        'published',
+    ];
+
+    protected $casts = [
+        'scheduled_publish_at' => 'datetime',
+        'published' => 'boolean',
+    ];
+
+    public function prompt()
+    {
+        return $this->belongsTo(Prompt::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->hasOne(Post::class);
+    }
 }
