f3fc9e05c31d66e8a1b1cac472b33faf5c5a27e5
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-09 21:42:33 +0100

    nodige imports toegevoegd en een default admin user gemaakt met gehashed wachtwoord

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,6 +26,14 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        // Create default admin user
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@ehb.be',
+            'password' => Hash::make('Password123!'),
+            'is_admin' => true,
         ]);
     }
 }
