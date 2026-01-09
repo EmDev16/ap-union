8a60b66ca5e9bd8c92e634e1b0ad3a23828a321d
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-09 22:47:58 +0100

    Routes aangepast zodat ProfileController als resource wordt gebruikt met alleen de methoden show, edit en update.

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('profiles', ProfileController::class)->only(['show']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('profiles', ProfileController::class)->only(['edit', 'update', 'destroy']);
 });
 
 require __DIR__.'/auth.php';
