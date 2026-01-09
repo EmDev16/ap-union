d028c673dd355cc1e5c0161832f92878ec572344
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-09 22:30:54 +0100

    Post model met relationships naar User, likes, en comments met gebruik van polymorphic relationships gemaakt

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'type',
+        'content',
+        'media_path',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
 }
