ac95b51c03a832bdf3fb0acbdd6362937918bdd1
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-09 22:40:48 +0100

    De EnsureAdmin middleware in de route middleware array geregistreerd

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
 
         // Admin middleware (registreer hier)
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'is_admin' => \App\Http\Middleware\EnsureAdmin::class,
     ];
 }
