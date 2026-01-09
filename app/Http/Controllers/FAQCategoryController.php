ba8e9c218575b9fbad7a567795ccc839a7e1c0d7
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-09 22:03:37 +0100

    Basis modellen met migrations en resource controllers gemaakt met commands

---
+++ app/Http/Controllers/FAQCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FAQCategory;
+use Illuminate\Http\Request;
+
+class FAQCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FAQCategory $fAQCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FAQCategory $fAQCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FAQCategory $fAQCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FAQCategory $fAQCategory)
+    {
+        //
+    }
+}
